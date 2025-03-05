17048342704bb956dea24e988090665efb9fbeed
xBrunodevx <brunofernandesfeitosa1@gmail.com>
2025-03-05 15:51:49 -0300

    auterndo html css e js

--- script.js
+++ script.js
@@ -18,19 +18,6 @@ function proximaImg() {
   document.getElementById("radio" + cont).checked = true;
 }
 
-var onda1 = document.getElementById("onda1");
-var onda2 = document.getElementById("onda2");
-var onda3 = document.getElementById("onda3");
-var onda4 = document.getElementById("onda4");
-
-window.addEventListener("scroll", function () {
-  var rolagemPos = window.scrollY;
-  onda1.style.backgroundPositionX = 400 + rolagemPos * 4 + "px";
-  onda2.style.backgroundPositionX = 300 + rolagemPos * -3 + "px";
-  onda3.style.backgroundPositionX = 200 + rolagemPos * 2 + "px";
-  onda4.style.backgroundPositionX = 100 + rolagemPos * -1 + "px";
-});
-
 type =
   "application/ld+json" >
   {
